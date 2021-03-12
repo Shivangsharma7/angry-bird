7747b453793cd275f6e92c45fa530590cc8af56a
shivangsharma7 <owlshivi1234@gmail.com>
2021-03-12 18:08:58 +0530

    change in sketch.js

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg, platform;
+var log6;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -33,6 +34,9 @@ function setup() {
     box5 = new Box(810, 160, 70, 70);
     log4 = new Log(760, 120, 150, PI / 7);
     log5 = new Log(870, 120, 150, -PI / 7);
+    log6 = new log(100, 100, 150, PI);
+
+
 
     bird = new Bird(100, 100);
     platform = new Ground(150, 305, 300, 170);
@@ -58,6 +62,7 @@ function draw() {
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
     bird.display();
     platform.display();
